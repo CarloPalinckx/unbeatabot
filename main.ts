e8cdb374a6c8c2faf4b054b32ea2143fa820b8c3
Carlo Palinckx <carlopalinckx@gmail.nl>
2023-02-15 22:20:05 +0100

    update

--- main.ts
+++ main.ts
@@ -1,18 +1,24 @@
+/**
+ * NameSpaces
+ */
 namespace Cache {
-  export class InMemory {
-    getSome() {
-      return "some";
-    }
-  }
+    export class InMemory {}
 }
 
+namespace Positioning {
+    export class Position {
+        public x: number = 0;
+        public y: number = 0;
+
+        public move(x: number, y: number) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+
+const position = new Positioning.Position();
+
 basic.forever(function () {
-  console.log(new Cache.InMemory().getSome());
-  basic.showLeds(`
-	. . . . .
-	. . . . .
-	. . # . .
-	. . . . .
-	. . . . .
-	`);
+    basic.showString(`${position.x},${position.y}`);
 });
